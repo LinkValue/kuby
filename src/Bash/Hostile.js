fa48da588cc11e1b54c1a119497d97a10e03e441
Henri d'Auvigny <henri.dauvigny@gmail.com>
2018-03-23 17:39:11 +0100

    Remove js file

--- src/Bash/Hostile.js
+++
@@ -1,9 +0,0 @@
-const hostile = require('hostile');
-hostile.set('127.0.0.1', 'peercdn.com', function (err) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log('set /etc/hosts successfully!');
-    }
-});
